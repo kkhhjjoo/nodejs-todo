dfcc4c98116ddcbc4d9a19f1cf77ab15f19adba4
kkhhjjoo <khjoo1203@gmail.com>
2026-02-06 11:57:58 +0900

    배포하기: 데이터 베이스 배포

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const indexRouter = require('./routes/index');
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/api", indexRouter)
 const mongoURI = process.env.MONGODB_URI;
+
+console.log('mongouri', mongoURI);
 
 mongoose.connect(mongoURI).then(() => { console.log('mongoose connected') }).catch((err) => { console.log('DB connection fail', err) });
 
