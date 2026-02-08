f858734717b9b9a6342c3c6a5ab9d32f13e138be
kkhhjjoo <khjoo1203@gmail.com>
2026-02-08 16:07:43 +0900

    cors수정

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
 const taskApi = require('./task.api');
 const userApi = require('./user.api');
+
+const app = express();
+
+// CORS 설정 - 이 부분을 수정하세요!
+const corsOptions = {
+  origin: [
+    'https://todoapp-hj.netlify.app',  // 프론트엔드 URL
+    'http://localhost:3000'             // 로컬 개발용
+  ],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
 
 router.use('/tasks', taskApi);
 router.use('/user', userApi);
